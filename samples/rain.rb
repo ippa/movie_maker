92c8fb82212ed1d4142049d227aa5cf0358b071a
ippa <iplord@iplord.com>
2008-08-13 21:38:15 +0200

    rain.rb example upgraded to new syntax

--- samples/rain.rb
+++ samples/rain.rb
@@ -9,7 +9,7 @@ Rubygame.init()
 Surface.autoload_dirs = [ "media" ]
 	
 @screen = Screen.set_mode([800, 600], 0)
-movie = Movie.new(:screen => @screen, :background => Color[:black], :target_framerate => 200)
+@movie = Movie.new(:screen => @screen, :background => Color[:black], :target_framerate => 200)
 
 #
 # generate 5 seconds of rain, each raindrop moving from top to bottom in 1 second (1000ms), 50ms between each drop.
@@ -20,7 +20,7 @@ movie = Movie.new(:screen => @screen, :background => Color[:black], :target_fram
 	stop_at = nr / 20.0 + 1
 	
 	@raindrop = Sprite.new("raindrop.png")	
-	movie.between(start_at, stop_at).move(@raindrop, :from => [x,0], :to => [x,650])
+	@movie.resource(@raindrop).move([x,0]).between(start_at, stop_at).move([x,650])
 end
 
-movie.play
+@movie.play
